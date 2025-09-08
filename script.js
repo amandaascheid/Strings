df0ac9c2e126adc2d7ea61e509029e60f2743767
amandaascheid <scheid.amanda@escola.pr.gov.br>
2025-09-08 11:57:48 +0000

    ok

--- script.js
+++ script.js
@@ -14,6 +14,13 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
+
+  for (let i in palavras) {
+    palavras [i] = palavras [i].tolowerCase();
+  }
+
+  palavras = tiraPalavrasRuins(palavras);
+
   const frequencias = contaFrequencias(palavras);
   let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
@@ -21,8 +28,7 @@ function processaTexto(texto) {
     return frequencias[p2] - frequencias[p1];
   }
 
-  console.log(ordenadas)
-  return ordenadas.slice(0, 10);
+   return ordenadas.slice(0, 10);
 }
 
 function contaFrequencias(palavras) {
@@ -30,24 +36,21 @@ function contaFrequencias(palavras) {
   for (let i of palavras) {
     frequencias[i] = 0;
      for (let j of palavras) {
-      if (i == j) {
-        frequencias[i]++;
+         if (i == j) {
+             frequencias[i]++;
+          }
+        }
       }
-    }
-  }
    return frequencias;
 }
 
 function tiraPalavrasRuins(palavras) {
-  const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
-
-  const palavrasBoas = [];
-
-  for (let palavra of palavras) {
-    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
-      palavrasBoas.push(palavra);
+  const PALAVRAS_RUINS = new Set (["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+         palavrasBoas.push(palavra);
     }
   }
-
-  return palavrasBoas;
+     return palavrasBoas;
 }
